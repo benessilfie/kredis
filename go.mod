185594ed819906e51afb15117f5467f6046d4467
Essilfie <bquansah007@gmail.com>
2024-08-18 23:54:51 +0000

    setup tcp listener

---
+++ go.mod
@@ -0,0 +1,3 @@
+module kredis.essilfie.co.uk
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Listening on port :6379")
+
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+}
